84848817cb0a1f5bb7acaecbc23669d7febb420a
tfreitasf <tfreitasf@gmail.com>
2022-10-22 22:36:40 -0300

    created BMI layout

--- app/src/main/java/br/com/povengenharia/fitnesstracker/MainActivity.kt
+++ app/src/main/java/br/com/povengenharia/fitnesstracker/MainActivity.kt
@@ -1,11 +1,25 @@
 package br.com.povengenharia.fitnesstracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnImc:LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnImc = findViewById(R.id.btn_bmi)
+
+        btnImc.setOnClickListener{
+            val i = Intent(this, BmiActivity::class.java)
+            startActivity(i)
+
+        }
+
     }
 }
